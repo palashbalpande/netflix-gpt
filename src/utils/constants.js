1e7874bc43c767e2515490aa71fe379b7f51ba01
palashbalpande <palashbalpande37@gmail.com>
2024-08-04 21:01:33 +0530

    SignIn-SignUp form && Login Header comps

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const LOGO_URL = 
+    "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const BG_IMG_URL = 
+    "https://assets.nflxext.com/ffe/siteui/vlv3/826348c2-cdcb-42a0-bc11-a788478ba5a2/6d20b198-e7ab-4e9f-a1aa-666faa0298f9/IN-en-20240729-POP_SIGNUP_TWO_WEEKS-perspective_WEB_a67d8c9e-8121-4a74-98e4-8005eb2df227_large.jpg";
+
